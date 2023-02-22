29d2b62f15d38438f6048bf9650298616d9f5962
lrwestemeyer <89590391+lrwestemeyer@users.noreply.github.com>
2023-02-21 21:20:41 -0600

    Part 1

---
+++ Driver3.java
@@ -0,0 +1,12 @@
+package threepracticestuff;
+
+public class Driver3 {
+
+	public int Sum(int[] input) {
+		int sum = 0;
+		for (int i = 0; i < input.length; i++) {
+			sum += input[i];
+		}
+		return sum;
+	}
+}
