937a27ce3bba0bec38d608aa3165e59cc62d5a11
lrwestemeyer <89590391+lrwestemeyer@users.noreply.github.com>
2023-02-21 21:24:30 -0600

    Part 3- added main method

--- Driver3.java
+++ Driver3.java
@@ -1,6 +1,19 @@
 package threepracticestuff;
 
+import java.util.Scanner;
+
 public class Driver3 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("How many numbers would you like to input?");
+		int size = scan.nextInt();
+		int arr[] = new int[size];
+		System.out.println("Enter your array numbers now:");
+		for (int i = 0; i < size; i++) {
+			arr[i] = scan.nextInt();
+		}
+	}
+		
 
 	public int Sum(int[] input) {
 		int sum = 0;
